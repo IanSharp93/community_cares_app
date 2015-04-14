aba04f610668ea88c320f503e2f7275d2d95206f
Christian Alic Kelley <cakelley@colorado.edu>
2015-04-14 01:12:41 +0000

    Changed application page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-      if @user.save 
-
-      else
-        render 'new'
-      end
+     if @user.save 
+      # Handle a successful save.
+     else
+      render 'new'
+     end
   end  
+
+  private
+  
+     def user_params 
+       params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+     end
+
 end
