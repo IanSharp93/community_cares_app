badca08cf896d9526447a3f13199d9cb1a32d27b
Christian Alic Kelley <cakelley@colorado.edu>
2015-04-14 21:38:59 +0000

    Changed header.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,9 @@
 class UsersController < ApplicationController
-
+  
   def show
     @user = User.find(params[:id])
-    debugger 
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(params[:user])
-     if @user.save 
-      flash[:success] = "Welcome to Community Cares!"
-      redirect_to @user
-     else
-      render 'new'
-     end
-  end  
-
-  private
   
-     def user_params 
-       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-     end
-
+  def new
+  end
 end
